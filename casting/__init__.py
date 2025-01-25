bb09beeddbe0c51565db77ac32cd64bfc0945a25
chhunyeangseng <chhunyeangseng@gmail.com>
2025-01-25 10:16:57 +0700

    Casting

---
+++ casting/__init__.py
@@ -0,0 +1,13 @@
+# Casting Datatype
+a=2.3
+b=24
+string="20"
+print("Casting Float to Integer",{a},"=",int(a))
+print("Casting Integer to Float  ",{b},"=",float(b))
+print("Casting String to Integer",{string},"=",int(string))
+print("="*40)
+print("Convert String to Integer ".center(30))
+print("="*40)
+
+convertStringToInt=int(string)
+print("Result",int(convertStringToInt+b))
